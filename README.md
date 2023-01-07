6fcbeca75224026c9982edcd865abdd236d57d0a
LongEarW <48570057+LongEarW@users.noreply.github.com>
2023-01-06 18:18:51 -0800

    Add project skeleton

---
+++ README.md
@@ -0,0 +1,77 @@
+# ETL off a SQS Queue
+The ETL off a SQS Queue implements the whole ETL process through Airflow. And it will lauch a new run every 5 minutes.
+The ETL consists 3 steps: 
+**Data Extraction**: use BashOperator to extract data through awscli-local from SQS
+**Data Cleaning**: flattern nested Json data, convert to Pandas dataframe; mark PII and convert app version to integer; save the transformed data into csv file.
+**Data Storage**: insert the csv file into Postgres.
+
+
+## Table of contents
+- Requirements
+- Installation
+- Maintenance
+- Answer to Questions
+## Requirements
+This ETL requires the following modules:
+- [Docker]
+- [Docker Compose]
+
+## Installation
+1. Clone the repo, enter the directory in terminal and build the services
+```
+docker compose up -d
+```
+2. Make sure every service is healthy by checking:
+```
+docker compose ps
+```
+1. Move the `sqs_dag.py` in `/dags`;
+2. In the Airflow dashboard (http://localhost:8080/), create the PostgreSQL connection at `Admin>Connections>ADD+`: 
+  - Connection Id: postgres_sqs
+  - Connection Type: Postgres
+  - Host: postgres_sqs
+  - Login: postgres
+  - Port: 5432  
+  <img src="./CreateConnection.png" width="200">
+1. Turn on the toggle of DAG: `sqs_msg_etl`. Within 5 minutes, a DAG runs will complete. 
+<img src="./TurnOnToggle.png" width="200">
+1. Check the PostgresSQL if the data inserted:
+- enter the container and the postgres CLI
+    ```
+    docker exec -it airflow-postgres_sqs-1 /bin/bash
+    psql -Upostgres
+    ```
+- In postgres CLI, connect to database `postgres` and query on the table
+  ```
+  \c postgres
+  SELECT * FROM user_logins LIMIT 10;
+  ```
+  ![DatabaseResult](./DatabaseResult.png)
+
+## Maintenance
+In the Airflow dashboard (http://localhost:8080/), check `Grid` and `Calendar` for overview of runs' status, and check log for detialed process.
+<img src="./Grid.png" width="200">
+
+## Answer to Questions
+  - **Q1. How would you deploy this application in production?**
+  Launch Airflow workers on multi-node cluster (Kubernetes cluster) to surpport scale. Also, switch interaction with LocalStack to AWS.
+<br/>
+  - **Q2. What other components would you want to add to make this production ready?**
+    1. Additional handling for extracting no message from SQS (current implementation can handle the case, but not elegantly).
+    2. Rebuilt the Airflow Worker image (current image doesn't include all the requires modules, they are installed at docker compose step, which is not suitable for production enviroment).
+    3. Add alarm notification for task failures.
+<br/>
+  - **Q3. How can this application scale with a growing dataset.**
+  If the SQS has high throughput, ETL may increase workflow frequency, limit the number of message for each consumption, increase number of work nodes and transfer the database from Postgres to distributed cluster (Hadoop, Redis).
+<br/>
+  - **Q4. How can PII be recovered later on?**
+  Given that the ETL encrypt the PII through ECB and base64, the user can recover the PII through reversed process:
+    ```
+    def decypt_text(en_text):
+        aes = AES.new(FETCH_TOKEN.encode("utf8"), AES.MODE_ECB)
+        base64_decrypted = base64.decodebytes(en_text.encode(encoding='utf-8'))
+        decrypted_text = str(aes.decrypt(base64_decrypted), encoding='utf-8').replace('\f', '')
+        return decrypted_text
+    ```
+  - **Q5. What are the assumptions you made?**
+  Assumed the version format follows sematic versioning.
---
+++ sqs_dag.py
@@ -0,0 +1,104 @@
+from airflow.decorators import dag, task
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.bash import BashOperator
+
+import pandas as pd
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+
+from datetime import datetime
+import json
+import base64
+
+FETCH_TOKEN = "FETCH_TOKEN_1234"  # encyption token
+MAX_VERSION_BIT = 8   # max sub version length, according to sematic versioning: x.y.z, x,y,z <= MAX_VERSION_BIT
+START_DATE = datetime(2023, 1, 1)  # date the workflow start
+DAG_SCHEDULE = '*/5 * * * *'  # runs frequency, defualt is every 5 minutes
+
+"""
+The function accecpts a string and make encyption with ECB. 
+Return an encypted and decoded string
+@param text (string)
+@return encypted text
+"""
+def encypt_text(text):
+    aes = AES.new(FETCH_TOKEN.encode("utf8"), AES.MODE_ECB)
+    encypt_text = aes.encrypt(pad(text.encode("utf8"), 16))
+    encypt_text = str(base64.encodebytes(encypt_text), encoding='utf-8').replace('\n', '')
+    return encypt_text
+
+"""
+The fucntion converts accept a version string and convert into version number with bit mask.
+The sub version should be within the range of MAX_VERSION_BIT to ensure comparable and recover of the converted version number.
+@param version (string)
+@return version number (integer)
+"""
+def version_to_integer(version):
+    version_bits = version.split('.')
+    if len(version_bits) == 3:
+        version_number_x = int(version_bits[0]) << (MAX_VERSION_BIT * 2) 
+        version_number_y = int(version_bits[1]) << MAX_VERSION_BIT
+        version_number_z = int(version_bits[2])
+    elif len(version_bits) == 2:
+        version_number_x = int(version_bits[0]) << (MAX_VERSION_BIT * 2) 
+        version_number_y = int(version_bits[1]) << MAX_VERSION_BIT
+        version_number_z = 0
+    elif len(version_bits) == 1:
+        version_number_x = int(version_bits[0]) << (MAX_VERSION_BIT * 2) 
+        version_number_y = 0
+        version_number_z = 0
+    return version_number_x | version_number_y | version_number_z
+    
+"""
+The DAG decorator for the ETL workflow
+@param start_date (datetime)
+@param START_DATE (datetime)
+@param catchup (boolean): if catch up history runs since start_date
+@param schedule: CRON expression schedule for the workflow
+"""
+@dag(
+    dag_id='sqs_msg_etl',
+    start_date=START_DATE,
+    catchup=False,
+    schedule=DAG_SCHEDULE
+)
+def tasks_flow():
+    # extract data from SQS, the output is concatenated into single line for inter-task transmission
+    extract_msg = BashOperator(
+        task_id="extract_msg",
+        bash_command="awslocal sqs receive-message  --queue-url http://localhost:4566/000000000000/login-queue --endpoint-url http://localstack:4566/  | tr '\n' ' '"
+    )
+
+    # data cleaning
+    @task(task_id='process_msg')
+    def process_extracted_user(msg_infos):
+        # first json parse: string to dataframe with nested columns
+        processed_msg = pd.json_normalize(data=json.loads(msg_infos)["Messages"])  
+        # second json parse: select and flatten nested columns
+        processed_msg = processed_msg['Body'].apply(lambda x: json.loads(x))
+        processed_msg = pd.DataFrame(list(processed_msg))
+        # fields selection and transformation
+        processed_msg['masked_ip'] = processed_msg['ip'].apply(encypt_text)
+        processed_msg['masked_device_id'] = processed_msg['device_id'].apply(encypt_text)
+        processed_msg['app_version'] = processed_msg['app_version'].apply(version_to_integer)
+        processed_msg = processed_msg[['user_id', 'device_type', 'masked_ip', 'masked_device_id', 'locale', 'app_version']]
+        processed_msg['create_date'] = datetime.now()
+        # store data as csv file for further operation
+        processed_msg.to_csv('/tmp/processed_msg.csv', index=None, header=False)
+
+    # create hook for postgres of SQS messages
+    postgres_hook = PostgresHook(
+        postgres_conn_id="postgres_sqs"
+    )
+    # insert data into postgres
+    @task(task_id='store_msg')
+    def store_msg():
+        postgres_hook.copy_expert(
+            sql="COPY user_logins FROM stdin WITH DELIMITER as ','",
+            filename='/tmp/processed_msg.csv'
+        )
+    # specify task dependencies
+    extract_msg >> process_extracted_user(extract_msg.output) >> store_msg()
+
+sqs_etl_dag = tasks_flow()
